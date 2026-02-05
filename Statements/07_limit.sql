dc9d891b771d05f9b0c3c83d57d721d0266d30dd
jjkk <tabita2081@gmail.com>
2026-02-05 10:06:46 -0300

    feat actualizacion: LIMIT

--- Statements/07_limit.sql
+++ Statements/07_limit.sql
@@ -1,8 +1,9 @@
+-- 1. Selección con límite simple
 SELECT * FROM users 
-LIMIT 2;
--- Limita los primeros 2 datos de la tabla 
+LIMIT 2; 
 
-SELECT * FROM users WHERE NOT email = "sara@gmil.com" OR age = 15 LIMIT 2; 
--- para limitar paginaciones, etc, 
-
--- LIMIT [cantidad de filas] OFFSET [cuántas filas saltarse]
+-- 2. Selección con filtros y comillas estándar
+-- Usamos paréntesis para agrupar la lógica y comillas simples para el texto
+SELECT * FROM users 
+WHERE (NOT email = 'sara@gmail.com') OR age = 15 
+LIMIT 2 OFFSET 0;
